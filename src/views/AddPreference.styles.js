7910fbae84a31a2ea1716b03470e5ff9225b9230
Paweł Zajączkowski <pawel.zajaczkowski91@gmail.com>
2022-04-27 15:47:22 +0200

    Added form validation

--- src/views/AddPreference.styles.js
+++ src/views/AddPreference.styles.js
@@ -24,4 +24,10 @@ export const FormWrapper = styled.form`
   width: 320px;
   height: auto;
   margin: 4px 0;
+  p {
+    align-self: flex-start;
+    font-size: ${({ theme }) => theme.fontSize.s};
+    font-weight: ${({ theme }) => theme.fontWeight.sb};
+    color: ${({ theme }) => theme.colors.error};
+  }
 `;
